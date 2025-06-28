e53d2a94c31533a219a69d808d22033072dcbbe6
Valcet2505 <fernandobonifas25@gmail.com>
2025-06-27 21:10:43 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const app = express();
 app.use(cors({
      origin: ['https://frontend-vajw.vercel.app'],
      credentials: true
